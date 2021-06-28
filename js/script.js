b91d166de7b49c572adcdc7d25fdff6f8c19e0e7
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-06-28 12:11:15 +0000

    Added function of carousel at script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const slides = document.querySelectorAll('.slide');
 const nextBtn = document.querySelector('.nextBtn');
 const prevBtn = document.querySelector('.prevBtn');
 
-slides.forEach(function(slide) {
+slides.forEach(function(slide,index) {
     slide.style.left = `${index * 100}%`;
 
 });
@@ -19,4 +19,11 @@ prevBtn.addEventListener('click',function() {
     counter--;
     carousel();
 });
+
+// Function to Provide Carousel
+function carousel() {
+    slides.forEach(function(slide) {
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+}
 
