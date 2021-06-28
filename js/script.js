eb926fe301db0a488f4649f6ffa3079569836731
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-06-28 12:06:26 +0000

    Updated event listners at script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@
 const slides = document.querySelectorAll('.slide');
 const nextBtn = document.querySelector('.nextBtn');
 const prevBtn = document.querySelector('.prevBtn');
+
+slides.forEach(function(slide) {
+    slide.style.left = `${index * 100}%`;
+
+});
+
+let counter = 0;
+
+nextBtn.addEventListener('click',function() {
+    counter++;
+    carousel();
+});
+
+prevBtn.addEventListener('click',function() {
+    counter--;
+    carousel();
+});
+
